6910b5aa055e1d6515ae92a47a78d8e2321f4ca0
PomiHD <wsgddjy@live.com>
2024-02-03 03:19:50 +1000

    115. Styled Components: Pseudo Selectors, Nested Rules & Media Queries

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -1,4 +1,4 @@
-﻿import { SetStateAction, useState } from "react";
+﻿import React, { SetStateAction, useState } from "react";
 import { styled } from "styled-components";
 
 const ControlContainer = styled.div`
@@ -33,6 +33,31 @@ const Input = styled.input<myProps>`
     0 1px 3px 0 rgba(0, 0, 0, 0.1),
     0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+const TextButton = styled.button`
+  color: #f0b322;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    color: #f0920e;
+  }
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -82,12 +107,8 @@ export default function AuthInputs() {
         </p>
       </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <TextButton>Create a new account</TextButton>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
