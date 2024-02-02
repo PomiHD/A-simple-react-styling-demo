f4c26bebc547f23ebae8d9c3780015590e75f466
PomiHD <wsgddjy@live.com>
2024-02-02 15:55:45 +1000

    Revert ...
    This reverts commit e95ac70f3ffd9b080911b9b7bd278fab53435d2e.

--- src/mian.tsx
+++ src/mian.tsx
@@ -2,10 +2,5 @@
 
 import App from './App';
 import './index.css';
-import {DevSupport} from "@react-buddy/ide-toolbox";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<DevSupport ComponentPreviews={ComponentPreviews}
-                                                                        useInitialHook={useInitial}
->
-    <App/>
-</DevSupport>);
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
