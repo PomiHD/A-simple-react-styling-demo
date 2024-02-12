484c3f3efcf46d782d4d8d94f0bc13d556316bb0
Samuel Yin <wsgddjy@live.com>
2024-02-12 17:45:10 +1000

    Migrate project to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import Header from "./components/Header";
 import AuthInputs from "./components/AuthInputs";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <AuthInputs />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <AuthInputs />
+            </main>
+        </>
+    );
 }
+
